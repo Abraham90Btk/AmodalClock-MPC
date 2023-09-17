640f7338714279ee9241cc3767bd7be06e518137
Abraham90Btk <azahel90@gmail.com>
2023-09-16 22:58:19 -0700

    Add files

---
+++ Startup.m
@@ -0,0 +1,17 @@
+%Run this file to add the directories to your matlab path
+path = '..\Cell_Reports_Code';%
+
+
+addpath(genpath(fullfile(path,'data')))
+addpath(genpath(fullfile(path,'delete_data')))
+addpath(genpath(fullfile(path,'scripts')))
+addpath(genpath(fullfile(path,'Figures')))
+
+%Before you run this file, you should download following
+% repositories from MathWorks:
+% boundedline.m
+% platonic_solid.m
+% Circular Statistics Toolbox
+%TubPlot
+
+
---
+++ main_figures.m
@@ -0,0 +1,38 @@
+run Startup
+
+%Figure 1: B,C,E
+%% Modelo por orden serial
+pathTofolder = '.\data';
+pathToImages = '.\Figures';
+
+path = '.\Main_Scripts';
+
+
+%%
+%Figure 1 - Rhythmic tapping behavior.
+f1 = Behavior_analyses(pathTofolder);
+print(f1, '-dpdf', 'FigureS6_C.pdf','-bestfit');
+print(f1,fullfile(pathToImages,'Figure_1.png'),'-dpng','-r300');
+close(f1)
+%Figure 2 - Neural population trajectories.
+f2 = Trajectories_analyses(pathTofolder);
+print(f2,fullfile(pathToImages,'Figure_2.png'),'-dpng','-r300');
+close(f2)
+%Figure 3 - Kinematic of neural trajectories
+f3 = Kinematics_analysis(pathTofolder);
+print(f3,fullfile(pathToImages,'Figure_3.png'),'-dpng','-r300');
+close(f3)
+%Figure 4 - Properties of Neural Sequences.
+f4 = NeuralSequences_analyses(pathTofolder);
+print(f4,fullfile(pathToImages,'Figure_4.png'),'-dpng','-r300');
+close(f4)
+%Figure 5 - Dynamics of neural sequences.
+f5 = DNS_analyses(pathTofolder);
+print(f5,fullfile(pathToImages,'Figure_5.png'),'-dpng','-r300');
+close(f5)
+%Figure 6 - Generalizability of neural sequences. 
+ComputeDistanceDiagonalMatrices2023
+%%Figure 7 - . Neural sequence simulations.
+
+
+
